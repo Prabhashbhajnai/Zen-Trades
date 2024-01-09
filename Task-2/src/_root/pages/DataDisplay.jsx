095cdfd3fe246cb48a6e28664cc71f43bce354e2
Prabhashbhajani <prabhashbhajani@gmail.com>
2024-01-10 02:02:29 +0530

    Table added and data mapped

--- Task-2/src/_root/pages/DataDisplay.jsx
+++ Task-2/src/_root/pages/DataDisplay.jsx
@@ -4,10 +4,42 @@ import { useLocation } from 'react-router-dom'
 const DataDisplay = () => {
     const location = useLocation();
 
+    const columns = location.state.columns;
+
     console.log(location.state);
+    const res = location.state.data;
+    // console.log(res);
+    const products = Object.keys(res).map(key => ({ id: key, ...res[key] }))
+    products.sort((a, b) => b.popularity - a.popularity);
+    // console.log(products);
 
     return (
-        <div>DataDisplay</div>
+        <div className='w-screen p-10 flex justify-center items-center'>
+            <table className='border-2 w-full'>
+                <tr className='border-2'>
+                    {columns.map((column, index) => (
+                        <th 
+                            key={index}
+                            className='w-1/4 border-2'
+                        >
+                            {column}
+                        </th>
+                    ))}
+                </tr>
+                {products.map((product, index) => (
+                    <tr className='border-2 text-wrap text-center'>
+                        {columns.map((column, index) => (
+                            <td 
+                                key={index}
+                                className='text-wrap border-2'
+                            >
+                                {product[column.toLowerCase()]}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </table>
+        </div>
     )
 }
 
